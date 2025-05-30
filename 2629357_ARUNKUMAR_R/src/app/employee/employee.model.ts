ae0632947aa44edfe2f2070f87bc16412e085b31
ARUNKUMAR <145777038+arun3644@users.noreply.github.com>
2025-05-30 12:54:02 +0530

    Add files via upload

---
+++ 2629357_ARUNKUMAR_R/src/app/employee/employee.model.ts
@@ -0,0 +1,6 @@
+export interface Employee{
+    id:number | String,
+    name:string,
+    emailId:string,
+    phoneNumber:number
+}
---
+++ 2629357_ARUNKUMAR_R/src/app/service/employee.service.ts
@@ -0,0 +1,96 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Employee } from '../employee/employee.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  private readonly URL = "https://cnnj374klfbinceonr3wa46we4.appsync-api.eu-north-1.amazonaws.com/graphql";
+  private readonly API_KEY = "da2-rmyffoooqjegbp467zna6ugma4";
+
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'x-api-key': this.API_KEY
+  });
+
+  constructor(private http: HttpClient) {}
+
+  getAllEmployee(): Observable<Employee[]> {
+    const query = `
+      query {
+        listEmployees {
+          emailId
+          id
+          phoneNumber
+          name
+        }
+      }
+    `;
+    return this.http.post<any>(this.URL, { query }, { headers: this.headers }).pipe(
+      map(res => res.data.listEmployees)
+    );
+  }
+
+  getEmployee(id: number): Observable<Employee> {
+    const query = `
+      query GetEmployee($id: Int!) {
+        getEmployee(id: $id) {
+          emailId
+          id
+          name
+          phoneNumber
+        }
+      }
+    `;
+    return this.http.post<any>(
+      this.URL,
+      { query, variables: { id } },
+      { headers: this.headers }
+    ).pipe(
+      map(res => res.data.getEmployee)
+    );
+  }
+
+  add(emp: Employee): Observable<Employee> {
+    const mutation = `
+      mutation AddEmployee($emp: EmployeeInput!) {
+        addEmployee(emp: $emp) {
+          emailId
+          id
+          name
+          phoneNumber
+        }
+      }
+    `;
+    return this.http.post<any>(
+      this.URL,
+      { query: mutation, variables: { emp } },
+      { headers: this.headers }
+    ).pipe(
+      map(res => res.data.addEmployee)
+    );
+  }
+
+  deleteEmployee(id: number): Observable<Employee> {
+    const mutation = `
+      mutation DeleteEmployee($id: Int!) {
+        deleteEmployee(id: $id) {
+          emailId
+          id
+          name
+          phoneNumber
+        }
+      }
+    `;
+    return this.http.post<any>(
+      this.URL,
+      { query: mutation, variables: { id } },
+      { headers: this.headers }
+    ).pipe(
+      map(res => res.data.deleteEmployee)
+    );
+  }
+}
+
